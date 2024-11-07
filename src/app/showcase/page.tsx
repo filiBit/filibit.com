07943b23cfddeb5c66efe818f8b110e402ab2bf7
filibit <filip.biterski@gmail.com>
2024-11-07 07:12:47 +0100

    style(showcase page): update content

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -13,17 +13,42 @@ export default function Showcase() {
                 &gt; Showcase
             </div>
             <div className="px-2 md:px-4">
-                <h1 className="h1 text-center mt-8 mb-0">Showcase</h1>
-                <p className="mb-8 mt-1 text-center">
-                    A decade of experience in building unique and custom Web
-                    applications
-                </p>
+                <h1 className="h1 text-center mt-8 mb-8">Showcase</h1>
                 <div className="col">
+                    <div className="my-4 col md:row gap-3 align-start">
+                        <div className="md:x-span-3 grow">
+                            <h2 className="h2">Oxiris CSS</h2>
+                            <p>A CSS framework for building nice GUIs fast.</p>
+                            <p>
+                                Wide set of utility and component classes are
+                                available for a good developer experience and
+                                consistency with design language.
+                            </p>
+                            <ul>
+                                <li>
+                                    App:{" "}
+                                    <a
+                                        href="https://github.com/filiBit/oxiris-css"
+                                        className="break-all link"
+                                    >
+                                        https://github.com/filiBit/oxiris-css
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="max-x-span-4 w-100% grow row border-1px mx-1 sm:mx-2 md:mx-0 align-center justify-center round-1 overflow-no">
+                            <img
+                                src="/oxiris-css-preview.png"
+                                className="w-100% filter-low hover:filter-no"
+                                alt="Screenshot of a design done with Oxiris CSS"
+                            />
+                        </div>
+                    </div>
                     <div className="my-4 col md:row gap-3 align-start">
                         <div className="md:x-span-3 grow">
                             <h2 className="h2">Abstraxo</h2>
                             <p>
-                                A Web application for generating mind maps and
+                                A Web app for generating mind maps and
                                 visualizing structured information.
                             </p>
                             <p>
@@ -82,7 +107,7 @@ export default function Showcase() {
                         <h2 className="h2">must-be-valid.js</h2>
                         <p>
                             Another developer tool for runtime data validation
-                            written in Typescript
+                            written in Typescript.
                         </p>
                         <ul>
                             <li>
@@ -119,24 +144,21 @@ export default function Showcase() {
                         <p>
                             Over 4 years of professional experience as a
                             Software developer, primarily as a Frontend
-                            developer with tendency for Fullstack.
+                            developer with a tendency for Fullstack.
                         </p>
                         <p>My tech stack includes:</p>
                         <ul>
-                            <li>HTML and CSS</li>
+                            <li>SQLite</li>
+                            <li>JSON:API and GraphQL</li>
+                            <li>Next.js, React, Angular and Solid</li>
+                            <li>Node and Deno</li>
                             <li>Javascript and Typescript</li>
-                            <li>React</li>
-                            <li>Angular</li>
-                            <li>Solid</li>
-                            <li>Next.js</li>
-                            <li>GraphQl</li>
-                            <li>JSON:API</li>
-                            <li>MongoDb</li>
+                            <li>HTML and CSS</li>
                         </ul>
                     </div>
 
                     <div className="col align-center my-8">
-                        <p className="m-0">Do you need a Web application?</p>
+                        <p className="m-0">Do you need a Web app?</p>
                         <div></div>
                         <Link href="/contact" className="button h-6 mt-3">
                             Contact me
