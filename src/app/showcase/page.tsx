72e337bfda178a0f2eaccf39853bc14cc6fe706f
Filip Biterski <filip.biterski@filibit.com>
2025-02-05 13:27:05 +0100

    refactor: reorder showcase page content

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -45,41 +45,6 @@ export default function Showcase() {
                             </a>
                         </div>
                     </div>
-                    <div className="border-1px round-1 align-start overflow-no hue-d chroma-bg-2">
-                        <h2 className="m-0 my-2 h3 row align-center px-2 w-100%">
-                            Abstraxo
-                        </h2>
-                        <div className="col md:row gap-3 md:gap-2 align-start my-2 px-2">
-                            <div>
-                                <p className="m-0 mb-2">
-                                    A Web app for generating mind maps and
-                                    visualizing structured information.
-                                </p>
-                                <p className="my-2">
-                                    It supports manual drawing and
-                                    auto-generation from different sources, as
-                                    well as creating relations between the
-                                    nodes.
-                                </p>
-                            </div>
-
-                            <div className="max-w-span-5 md:max-w-span-4 row border-1px align-center justify-center round-1 overflow-no">
-                                <img
-                                    src="/abstraxo-preview.png"
-                                    className="w-100% filter-low hover:filter-no"
-                                    alt="Screenshot of the Abstraxo app"
-                                />
-                            </div>
-                        </div>
-                        <div className="row px-2 pb-2 pt-2 md:pt-0">
-                            <a
-                                href="https://abstraxo.com"
-                                className="button var-contained align-self-start h-5 f-size-sm"
-                            >
-                                &gt; App
-                            </a>
-                        </div>
-                    </div>
                     <div className="border-1px round-1 col align-start overflow-no hue-d chroma-bg-2">
                         <h2 className="m-0 h-span-1 h3 row align-center px-2 w-100%">
                             JSON type generator
@@ -140,6 +105,41 @@ export default function Showcase() {
                                 className="button var-contained align-self-start h-5 f-size-sm"
                             >
                                 &gt; Repository
+                            </a>
+                        </div>
+                    </div>
+                    <div className="border-1px round-1 align-start overflow-no hue-d chroma-bg-2">
+                        <h2 className="m-0 my-2 h3 row align-center px-2 w-100%">
+                            Abstraxo
+                        </h2>
+                        <div className="col md:row gap-3 md:gap-2 align-start my-2 px-2">
+                            <div>
+                                <p className="m-0 mb-2">
+                                    A Web app for generating mind maps and
+                                    visualizing structured information.
+                                </p>
+                                <p className="my-2">
+                                    It supports manual drawing and
+                                    auto-generation from different sources, as
+                                    well as creating relations between the
+                                    nodes.
+                                </p>
+                            </div>
+
+                            <div className="max-w-span-5 md:max-w-span-4 row border-1px align-center justify-center round-1 overflow-no">
+                                <img
+                                    src="/abstraxo-preview.png"
+                                    className="w-100% filter-low hover:filter-no"
+                                    alt="Screenshot of the Abstraxo app"
+                                />
+                            </div>
+                        </div>
+                        <div className="row px-2 pb-2 pt-2 md:pt-0">
+                            <a
+                                href="https://abstraxo.com"
+                                className="button var-contained align-self-start h-5 f-size-sm"
+                            >
+                                &gt; App
                             </a>
                         </div>
                     </div>
