080a388852d1e4b0c881c4680c9a74edb3724ba9
Filip Biterski <filip.biterski@filibit.com>
2025-05-21 22:10:12 +0200

    refactor: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { HomeCard } from "@/components/home-card";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -10,81 +11,89 @@ export default function Home() {
     return (
         <>
             <div className="px-2 md:px-4">
-                <h1 className="text-center h1 mt-8 mb-4">Welcome</h1>
-                <p>
-                    My name is Filip Biterski. I am a Web developer with many
-                    years of professional experience in Frontend.
-                </p>
-                <p>
-                    Software is my passion, so my free time is usually spent
-                    creating software products and open-source libraries.
-                </p>
-                <p className="my-8 text-center">Here are some of my skills</p>
-                <div className="my-2 md:my-4 row flex-wrap gap-6 md:gap-4 justify-center">
-                    <div className="w-span-5 grow border-1px round-1 overflow-no hue-d chroma-bg-3">
-                        <h2 className="row m-0 align-center justify-center my-2 h3 text-center">
-                            Languages
-                        </h2>
-                        <p className="my-3 px-3">
-                            Javascript, Typescript, JSDoc
-                        </p>
-                    </div>
-                    <div className="w-span-5 grow border-1px round-1 overflow-no hue-d chroma-bg-3">
-                        <h2 className="row my-2 align-center justify-center h3 text-center">
-                            Environments
-                        </h2>
-                        <p className="my-3 px-3">Browser, node, deno</p>
-                    </div>
-                    <div className="w-span-5 grow border-1px round-1 overflow-no hue-d chroma-bg-3">
-                        <h2 className="row my-2 align-center justify-center h3 text-center">
-                            Frontend
-                        </h2>
-                        <p className="my-3 px-3">
-                            React, Next.js, Angular 14+, Vue, Redux, Webpack,
-                            HTML, CSS
-                        </p>
-                    </div>
-                    <div className="w-span-5 grow border-1px round-1 overflow-no hue-d chroma-bg-3">
-                        <h2 className="row my-2 align-center justify-center h3 text-center">
-                            Backend
-                        </h2>
-                        <p className="my-3 px-3">
-                            SQL, SQLite, Express.js, JSON:API, GraphQL
-                        </p>
-                    </div>
-                    <div className="w-span-5 grow border-1px round-1 overflow-no hue-d chroma-bg-3">
-                        <h2 className="row my-2 align-center justify-center h3 text-center">
-                            Software design
-                        </h2>
-                        <p className="my-3 px-3">
-                            Design principles and design patterns
-                        </p>
-                    </div>
-                    <div className="w-span-5 grow border-1px round-1 overflow-no hue-d chroma-bg-3">
-                        <h2 className="row my-2 align-center justify-center h3 text-center">
-                            More
-                        </h2>
-                        <p className="my-3 px-3">
-                            Checkout my CV{" "}
-                            <a href="/CV.pdf" className="link">
-                                here
-                            </a>
-                        </p>
-                    </div>
+                <h1 className="text-center h1 mt-8 mb-8">
+                    5 years of building digital products
+                </h1>
+                <div className="row flex-wrap gap-4 mb-8">
+                    <HomeCard
+                        title="Web solutions"
+                        description="Building aesthetic and optimized user interfaces"
+                        tags={["SEO", "Accessibility", "Responsive design"]}
+                    />
+                    <HomeCard
+                        title="Server"
+                        description="Running your business logic and automating tasks"
+                        tags={["APIs", "REST", "GraphQL"]}
+                    />
+                    <HomeCard
+                        title="Database"
+                        description="Supporting data needs of your business with apropriate database design and physical implementation"
+                        tags={["Database design", "Relational model"]}
+                    />
+                    <HomeCard
+                        title="Expertise"
+                        description="Creating a maintainable software that follows industry standards for code and infrastructure"
+                        tags={[
+                            "design patterns",
+                            "design principles",
+                            "DDD architecture",
+                        ]}
+                    />
                 </div>
-                <div className="row flex-wrap justify-center gap-2 md:gap-4 align-center mt-8 mb-8">
+                <div className="row flex-wrap justify-center gap-2 md:gap-4 align-center mt-8 mb-8 border-1px py-6 px-2 chroma-bg-2 hue-d">
                     <Link
                         href="/showcase"
-                        className="button var-contained w-span-4 hue-d h-6"
+                        className="button var-contained w-span-4 h-6 chroma-bg-0"
                     >
                         Explore showcase
                     </Link>
                     <Link
                         href="/contact"
-                        className="button var-contained w-span-4 hue-d h-6"
+                        className="button var-contained w-span-4 h-6 chroma-bg-0"
                     >
                         Contact me
                     </Link>
+                </div>
+                <div className="row flex-wrap gap-4 mb-8">
+                    <div className="w-span-6 grow shrink">
+                        <h2 className="h2">
+                            Tech stack&nbsp;
+                            <span className="hue-b chroma-bg-2">*</span>
+                        </h2>
+
+                        <div className="col gap-2">
+                            <div className="border-1px p-1">HTML, CSS</div>
+                            <div className="border-1px p-1">
+                                Javascript, Typescript, JSDoc
+                            </div>
+                            <div className="border-1px p-1">node, deno</div>
+                            <div className="border-1px p-1">
+                                SQL, GraphQL, JSON:API
+                            </div>
+                            <div className="border-1px p-1">SQLite</div>
+                        </div>
+                        <p className="mt-2 mb-0 p-1 hue-b f-size-sm chroma-bg-2">
+                            * Software frameworks and other tools are not listed
+                            here since I know many and can quickly adapt to any
+                            one due to experience and a solid engineering
+                            foundation
+                        </p>
+                    </div>
+                    <div className="w-span-6 grow shrink">
+                        <h2 className="h2">Practices</h2>
+
+                        <div className="col gap-2">
+                            <div className="border-1px p-1">
+                                Software design principles and patterns
+                            </div>
+                            <div className="border-1px p-1">
+                                Domain-Driven Design (DDD) architecture
+                            </div>
+                            <div className="border-1px p-1">
+                                Agile and Waterfall workflows
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
