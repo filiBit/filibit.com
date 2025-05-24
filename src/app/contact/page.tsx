5b6e623218f9c1bdd7e2c6e341d498b7b404b445
Filip Biterski <filip.biterski@filibit.com>
2025-05-24 06:06:47 +0200

    feat: update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -19,32 +19,34 @@ export default function Contact() {
                         draggable="false"
                     />
                 </div>
-                <p className="m-0 lh-2 col gap-1">
-                    <div className="row">
-                        Name:&nbsp;
-                        <div className="select-all">Filip Biterski</div>
-                    </div>
-                    <div className="row">
-                        Phone:&nbsp;
-                        <div className="select-all">+385 95 732 0081</div>
-                    </div>
-                    <div className="row flex-wrap">
-                        Email:&nbsp;
-                        <div className="select-all">
-                            filip.biterski@filibit.com
+                <div className="col gap-2">
+                    <div className="m-0 col gap-1 align-start">
+                        <div className="select-all f-size-lg">
+                            Filip Biterski
+                        </div>
+                        <div className="row px-1 hue-b chroma-bg-2 lh-2">
+                            CEO / Software developer
                         </div>
                     </div>
-                    <div className="row align-center">
-                        CV:&nbsp;
-                        <a
-                            href="/CV.pdf"
-                            className="link row align-center justify-center h-3 w-3"
-                            target="_blank"
-                        >
-                            <ExternalLinkIcon className="max-h-2 max-w-2 w-100% h-100%" />
-                        </a>
+                    <div className="m-0 lh-2 col">
+                        <div className="row align-center">
+                            <span className="w-10">Email:</span>
+                            <div className="select-all f-size-sm">
+                                filip.biterski@filibit.com
+                            </div>
+                        </div>
+                        <div className="row align-center">
+                            <span className="w-10">CV:</span>
+                            <a
+                                href="/CV.pdf"
+                                className="link row align-center justify-center h-3 w-3"
+                                target="_blank"
+                            >
+                                <ExternalLinkIcon className="max-h-2 max-w-2 w-100% h-100%" />
+                            </a>
+                        </div>
                     </div>
-                </p>
+                </div>
             </div>
         </>
     );
